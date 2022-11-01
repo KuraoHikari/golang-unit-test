7a56570a1bb5f4e0e0454b4acd00eb1afec1056a
KuraoHikari <dewaindra705@gmail.com>
2022-11-01 21:57:11 +0800

    update all

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,19 +1,27 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestHelloWorld(t *testing.T){
+func TestHelloWorldEko(t *testing.T){
 	result := HelloWorld("Eko")
 	if result != "Hello Eko" {
 		// unit test failed
-		panic("Result is not Hello Eko")
+		// panic("Result is not Hello Eko")
+		// t.Fail()
+		t.Error("harusnya Hello Eko")
 	}
+	fmt.Println("Test HelloWorldEko Done")
 }
 
 func TestHelloWorldKurao(t *testing.T){
 	result := HelloWorld("Kurao")
 	if result != "Hello Kurao" {
 		// unit test failed
-		panic("Result is not Hello Kurao")
+		// t.FailNow()
+		t.Fatal("harusnya Hello Kurao YGY")
 	}
+	fmt.Println("Test HelloWorldKurao Done")
 }
